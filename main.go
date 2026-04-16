1891b0fe90dc92ce3ba78498bb4559ce19e6faa9
Piyush Tiwari <ithastobepiyush@gmail.com>
2026-04-16 14:29:44 +0530

    make command in go

--- main.go
+++ main.go
@@ -22,10 +22,40 @@ func main() {
 	// fmt.Printf("My family: %v", myFamily)
 
 	// slice-->
-	var myFriends []string
-	myFriends = append(myFriends, "Mike", "Adam")
-	fmt.Printf("My Frirends: %v", myFriends)
+	// var myFriends []string
+	// myFriends = append(myFriends, "Mike", "Adam")
+	// myFriends = append(myFriends, "Sam")
+	// fmt.Printf("My Frirends: %v", myFriends)
 
+	// multi dimenaional array
+	myCourses := [3][2]string{
+		{"go", "nodejs"},
+		{"aws", "gcp"},
+		{"cdk", "puluni"},
+	}
+	fmt.Printf("\nmyCourses()[3][2] : %v", myCourses)
+
+
+	// multi dimenaional slice array
+	mySliceCourses := [][]string{
+		{"aws", "gcp"},
+		{"cdk"},
+	}
+	course := []string{
+		"IAC",
+		"Puluni",
+	}
+	mySliceCourses = append(mySliceCourses, course)
+	mySliceCourses = append(mySliceCourses, []string{
+		"react",
+		"rust",
+		"react-native",
+		"python",
+	})
+	fmt.Printf("\n\nmySliceCourse()[][] : %v", mySliceCourses)
+
+	myBeCourses := make([]int, 2, 10)
+	
 
 
 	app.Listen("active on http://localhost:9000")
